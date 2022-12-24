004eea92eaf62adf7db754541d354a4c68f67a68
Lemon <62458905+lemonsama123@users.noreply.github.com>
2022-12-24 13:30:41 +0800

    update

---
+++ fontend/Vue/vue2/day3/demo-first/src/Test.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <div class="test-box">
+            <h3>这是用户自定义的 Test.vue --- {{ username }}</h3>
+            <button @click="changName">修改用户名</button>
+        </div>
+        <div>123</div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            username: 'zs'
+        }
+    },
+    methods: {
+        changName() {
+            this.username = '哇哈哈'
+        }
+    }
+}
+</script>
+
+<style lang="less">
+.test-box {
+    background-color: pink;
+    h3 {
+        color: red;
+    }
+}
+</style>
---
+++ fontend/Vue/vue2/day4/demo-1/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="app-container">
+    <h1>App 根组件</h1>
+    <hr/>
+
+    <div class="box">
+      <!-- 渲染 Left 组件和 Right 组件 -->
+      <Left/>
+      <Right/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Left from '@/components/Left.vue'
+import Right from '@/components/Right.vue'
+
+export default {
+  components: {
+    Left,
+    Right
+  }
+}
+</script>
+
+<style lang="less">
+.app-container {
+  padding: 1px 20px 20px;
+  background-color: #efefef;
+}
+
+.box {
+  display: flex;
+}
+</style>
